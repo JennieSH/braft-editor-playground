74d7eb713f13742b5c9a4c3a3d250945c33c46ee
Jennie <m65pjj@gmail.com>
2022-02-23 15:50:05 +0800

    feat: add editor

--- components/Editor.tsx
+++ components/Editor.tsx
@@ -1,8 +1,9 @@
-import 'braft-editor/dist/index.css'
-import { Dispatch, SetStateAction, useEffect, useState } from 'react'
-
 import BraftEditor, { EditorState } from 'braft-editor'
-import { LanguageConfigType, languageConfig } from '../const/editorLang'
+import { ContentUtils } from 'braft-utils'
+import { Dispatch, memo, SetStateAction, useCallback, useState } from 'react'
+
+import { languageConfig } from '../const/editorLang'
+import ImgUploaderBtn from './ImgUploaderBtn'
 
 interface Props {
   text: string
@@ -10,37 +11,71 @@ interface Props {
 }
 
 const Editor = ({ text, setText }: Props) => {
-  const [editorState, setEditorState] = useState<EditorState | undefined>(
-    BraftEditor.createEditorState(text)
-  )
+  const [editorState, setEditorState] = useState<EditorState>(BraftEditor.createEditorState(text))
 
   const onEditorStateChange = (editorState: EditorState) => {
     setEditorState(editorState)
     setText(editorState.toHTML())
   }
 
-  const languageFn = (languages: LanguageConfigType, context: 'braft-finder' | 'braft-editor') => {
-    // if (context === 'braft-finder') {
-    //   languages['zh-hant'].controls.media = '123'
-    // }
+  // 插入蘋果
+  const onInsertApple = useCallback(() => {
+    setEditorState(ContentUtils.insertText(editorState, ''))
+  }, [editorState])
 
-    return languageConfig
-  }
+  // 上傳圖片
+  const onUploadImg = useCallback(
+    (url: string) => {
+      setEditorState(
+        ContentUtils.insertMedias(editorState, [
+          {
+            type: 'IMAGE',
+            url
+          }
+        ])
+      )
+    },
+    [editorState]
+  )
 
   return (
     <BraftEditor
       value={editorState}
       onChange={onEditorStateChange}
-      controlBarStyle={{ boxShadow: 'none' }}
-      style={{}}
-      className="h-full"
-      placeholder="# braft"
-      controls={['bold', 'italic', 'link', 'media']}
-      language={languageFn}
+      controlBarClassName="editor-bar"
+      // controlBarStyle={{ boxShadow: 'none' }}
+      contentClassName="editor"
+      placeholder="# braft editor"
+      controls={[
+        'bold',
+        'italic',
+        'link',
+        'media',
+        'separator',
+        {
+          key: 'underline', // 使用 key 指定 controls 類型
+          title: '加個 Line ~', // tooltip title
+          text: <div>客製化底線</div> // 圖示，可傳入 JSX
+        },
+        {
+          key: 'insert-apple',
+          type: 'button',
+          title: '加個 ',
+          text: '',
+          onClick: onInsertApple
+        },
+        {
+          key: 'upload-img',
+          type: 'button',
+          title: '圖呢 ಠ·ಠ?',
+          text: <ImgUploaderBtn uploadCallback={onUploadImg} />
+        }
+      ]}
+      language={() => languageConfig}
       stripPastedStyles={true}
       media={{
         accepts: {
-          image: 'image/png,image/jpeg',
+          image: 'image/png,image/jpeg,image/webp',
           video: false,
           audio: false
         },
@@ -50,8 +85,9 @@ const Editor = ({ text, setText }: Props) => {
           audio: false
         }
       }}
+      imageControls={['remove']}
     />
   )
 }
 
-export default Editor
+export default memo(Editor)
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,11 +11,15 @@ const Braft: NextPage = () => {
   const [text, setText] = useState('')
 
   return (
-    <div className="flex gap-8 justify-center items-center min-h-screen px-28">
-      <div className="border rounded w-full h-[600px]">
+    <div className="flex flex-wrap md:flex-nowrap gap-8 justify-center items-center min-h-screen p-8 md:px-28">
+      <div className="border rounded w-full h-[400px] md:h-[600px]">
         <Editor text={text} setText={setText} />
       </div>
-      <textarea className="border rounded outline-none w-full h-[600px]" value={text} readOnly />
+      <textarea
+        className="border rounded outline-none p-4 w-full h-[400px] md:h-[600px] "
+        value={text}
+        readOnly
+      />
     </div>
   )
 }
