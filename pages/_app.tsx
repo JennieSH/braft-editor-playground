ad82deeb1c07881e83f6a7586c434de53643fa67
Jennie <m65pjj@gmail.com>
2022-02-23 15:48:05 +0800

    style: add editor css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
 import '../styles/globals.css'
+import 'braft-editor/dist/index.css'
+import '../styles/editor.css'
+
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="robots" content="noindex" />
+        <title>Braft Editor</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
