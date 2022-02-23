84891c6c270fbcc15dfdeb4a7898702d9affd871
Jennie <m65pjj@gmail.com>
2022-02-24 02:32:11 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+// import { useState } from "react";
+
+function App() {
+  return <div className="App">hello</div>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@/": path.resolve(__dirname, "./src"),
+      "@/components": path.resolve(__dirname, "./src/components"),
+      "@/styles": path.resolve(__dirname, "./src/styles"),
+      "@/utils": path.resolve(__dirname, "./src/utils"),
+      "@/assets": path.resolve(__dirname, "./src/assets"),
+      "@/pages": path.resolve(__dirname, "./src/pages"),
+      "@/routes": path.resolve(__dirname, "./src/routes"),
+      "@/hooks": path.resolve(__dirname, "./src/hooks")
+    }
+  }
+});
