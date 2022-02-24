e2270b147a3884c2f0c9d48cad41a14cc2863356
Jennie <m65pjj@gmail.com>
2022-02-24 13:43:32 +0800

    style: add index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-// import { useState } from "react";
+import Editor from "@/components/Editor";
+import "braft-editor/dist/index.css";
 import "@/styles/index.css";
+import { useState } from "react";
 
-function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+const App = () => {
+  const [text, setText] = useState("");
+
+  return (
+    <div className="flex flex-wrap md:flex-nowrap gap-8 justify-center items-center min-h-screen p-8 md:px-28">
+      <div className="border rounded w-full h-[400px] md:h-[600px]">
+        <Editor text={text} setText={setText} />
+      </div>
+      <textarea
+        className="border rounded outline-none p-4 w-full h-[400px] md:h-[600px] "
+        value={text}
+        readOnly
+      />
+    </div>
+  );
+};
 
 export default App;
